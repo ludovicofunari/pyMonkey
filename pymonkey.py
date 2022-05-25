1eb8e52eacd0450af9706fc8f67f72e95eaf218a
Ludovico Funari <ludovico.funari@gmail.com>
2022-05-25 21:18:49 +0200

    Fix typo

--- pymonkey.py
+++ pymonkey.py
@@ -13,13 +13,14 @@ def get_pod_name(item):
     else:
         return False, item
 
+
 def kill_pod(api, ret):
   while(len(ret.items)):
     item = random.choice(ret.items)
     chosen_item = get_pod_name(item)
     if chosen_item[0]:
         try:
-          print(f"# Killing pod: '{chosen_item[1]}'\tat {datetime.now()}")
+          print(f"# Killing pod: '{chosen_item[1]}' at {datetime.now()}")
           api.delete_namespaced_pod(chosen_item[1], "default")
         except Exception as err:
           print(f"Error deleting pod: {err}")
